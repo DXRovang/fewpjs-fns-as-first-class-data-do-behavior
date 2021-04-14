fa58fa0b62593664b12410b211c54d41ddd7f19c
Alexander Rovang <wolf_vii@yahoo.com>
2021-04-14 18:30:00 -0400

    commit tree 53aab1b1bb2c63ae5563db70ff053360ab2ba942

--- index.js
+++ index.js
@@ -10,4 +10,20 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+function greet(string) {
+  let value = parseInt(string.split(":")[0], 10)
+  if (value < 12){
+    return "Good Morning";
+  }
+  else if (value > 12 && value < 17){
+    return "Good Afternoon";
+  } 
+  else{
+    return "Good Evening";
+  } 
+}
 /* Write your implementation of displayMessage() */
+function displayMessage(string) {
+  let display = document.getElementById('greeting');
+  display.innerText = `${string}`;
+}
